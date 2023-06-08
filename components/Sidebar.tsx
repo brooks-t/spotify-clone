dbe81a179091545310c979314548fb258dad7042
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-06-08 14:49:03 -0400

    added home and search routes to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,27 @@
+"use client"; // because our sidebar is dynamic, we need to use client-side rendering.
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+
 interface SidebarProps {
     children: React.ReactNode;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({children}) => {
+    const pathname = usePathname();
+    const routes = useMemo(() => [
+        {
+            label: "Home",
+            active: pathname !== '/search',
+            href: "/",
+        },
+        {
+            label: "Search",
+            active: pathname === '/search',
+            href: "/search",
+        },
+    ], [pathname]); 
+
+    
     return ( 
         <div>
             {children}
